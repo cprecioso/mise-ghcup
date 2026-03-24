91ba77985ab4b58e8839ed867339ce8d76af5f14
Carlos Precioso <carlos@wasp-lang.dev>
2026-03-24 21:58:24 +0100

    Suppose installed ghcup

--- hooks/backend_install.lua
+++ hooks/backend_install.lua
@@ -18,18 +18,14 @@ function PLUGIN:BackendInstall(ctx)
     end
 
     local cmd = require("cmd")
-    local file = require("file")
     local log = require("log")
     local fs = require("fs")
-    local ghcup = require("ghcup")
-
-    local ghcup_bin, ghcup_env = ghcup.find_ghcup(cmd)
 
     -- Install the tool
     log.info("Installing " .. tool .. " " .. version .. " to " .. install_path)
 
     fs.mkdir_p(cmd, install_path)
-    cmd.exec(ghcup_bin .. " install " .. tool .. " " .. version .. " -i " .. install_path, { env = ghcup_env })
+    cmd.exec("ghcup install " .. tool .. " " .. version .. " -i " .. install_path)
 
     return {}
 end
